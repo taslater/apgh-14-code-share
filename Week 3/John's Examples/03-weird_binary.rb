58a4b973d809607ce4493bd9dca8840afcf33227
taslater <thomas.a.slater@gmail.com>
2022-04-18 14:40:44 -0400

    ruby has weird binary syntax

---
+++ Week 3/John's Examples/03-weird_binary.rb
@@ -0,0 +1,9 @@
+# I took Binary 101 last semester, but failed it miserably.
+# Turns out it was a level 5 course.
+
+for i in 0..2 do
+    for j in 0..10 do
+        puts "#{j}[#{i}] = #{j[i]}"
+    end
+    puts ""
+end
