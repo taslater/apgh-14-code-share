77759ed79362e333030ccd0c756af12c82fcd527
taslater <thomas.a.slater@gmail.com>
2022-04-18 15:08:26 -0400

    ruby for loop example

---
+++ Week 3/John's Examples/06-for_loops.rb
@@ -0,0 +1,18 @@
+# more like c/c++/c#
+for i in 1..10
+    puts i
+end
+
+# more ruby way
+(1..10).each do |i|
+    puts i
+end
+
+for i in 1..10 # 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    i = 100
+    puts i
+end
+
+([1, 5, 2, 8, 3, 4, 4, 4, 4, 4]).each do |i|
+    puts i
+end
